dee782629652575c53056449fd7407360dc7661b
Anders Pearson <anders@thraxil.org>
2023-03-08 14:12:03 +0000

    can we just override replit

--- replit.nix
+++ replit.nix
@@ -1,29 +1,19 @@
-{ pkgs }: {
-  deps = [
-    pkgs.python310Full
-    pkgs.replitPackages.prybar-python310
-    pkgs.replitPackages.stderred
-  ];
-  env = {
-    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      # Needed for pandas / numpy
-      pkgs.stdenv.cc.cc.lib
-      pkgs.zlib
-      # Needed for pygame
-      pkgs.glib
-      # Needed for matplotlib
-      pkgs.xorg.libX11
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/06278c77b5d162e62df170fec307e83f1812d94b.tar.gz") {} }:
 
-      # notebook and data science stuff
-      pkgs.jupyter
-      pkgs.python310.pkgs.numpy
-      pkgs.python310.pkgs.pandas
-      pkgs.python310.pkgs.psycopg2
-    ];
-    PYTHONHOME = "${pkgs.python310Full}";
-    PYTHONBIN = "${pkgs.python310Full}/bin/python3.10";
-    LANG = "en_US.UTF-8";
-    STDERREDBIN = "${pkgs.replitPackages.stderred}/bin/stderred";
-    PRYBAR_PYTHON_BIN = "${pkgs.replitPackages.prybar-python310}/bin/prybar-python310";
-  };
+pkgs.mkShell {
+  buildInputs = [
+  pkgs.jupyter
+  pkgs.python310
+  pkgs.python310.pkgs.numpy  
+  pkgs.python310.pkgs.pandas
+  pkgs.python310.pkgs.psycopg2
+
+      # Uncomment the following lines to make them available in the shell.
+      # pandas
+      # numpy
+      # matplotlib
+      ];
+
+  # Automatically run jupyter when entering the shell.
+  # shellHook = "jupyter notebook";
 }
